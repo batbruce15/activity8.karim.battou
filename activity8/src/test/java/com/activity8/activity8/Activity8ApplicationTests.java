2171e77ab0dc62686f0c66d497b09a3ffd73b21a
karim.battou20@gmail.com <karim.battou20@gmail.com>
2024-04-26 23:12:58 +0200

    activity8

---
+++ activity8/src/test/java/com/activity8/activity8/Activity8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity8.activity8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activity8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
